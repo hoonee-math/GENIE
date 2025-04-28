76d9a405a57e94907004a4828f1a9a88186aaa49
k-3730 <k_3730@naver.com>
2025-04-28 19:04:59 +0900

    FIX: logger.py 수정

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,9 @@
 import logging
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from logging.handlers import TimedRotatingFileHandler
+import threading
+import time
 
 def setup_logger():
     logger = logging.getLogger('Gemini_API') 
@@ -38,3 +40,21 @@ def setup_logger():
     return logger
 
 logger = setup_logger()
+
+# 자정마다 더미 로그 함수 및 스케줄러 추가
+def schedule_dummy_log():
+    now = datetime.now()
+    # 다음 자정 계산
+    next_midnight = (now + timedelta(days=1)).replace(hour=0, minute=0, second=1, microsecond=0)
+    delay = (next_midnight - now).total_seconds()
+
+    def log_and_reschedule():
+        logger.info('[DUMMY] 자정 더미 로그 - 로그 파일 회전 보장')
+        schedule_dummy_log()
+
+    timer = threading.Timer(delay, log_and_reschedule)
+    timer.daemon = True 
+    timer.start()
+
+# 스크립트 import 시 바로 스케줄 시작
+schedule_dummy_log()
