af46c83b925380dd0e3c3e39cf6abe96545518f4
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-07-10 15:46:41 +0900

    design: 지문 분석 layout 추가, 기존 PassageSummary 재활용

--- src/views/temp/GeneratedPassageView.vue
+++ src/views/temp/GeneratedPassageView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col gap-8 p-0 md:p-8 box-border w-full">
-        <PassageAndQuestionLayout>
+        <PassageAndQuestionLayout :left-ratio="2" :right-ratio="1" >
             <template #title>
                 passage.title
             </template>
@@ -9,17 +9,18 @@
             </template>
             <template #right>
                 generated_core_point
+                <PassageSummary>
+                    <template #subject>d</template>
+                    <template #keyword>d</template>
+                    <template #gistText>d</template>
+                </PassageSummary>
             </template>
-        </PassageAndQuestionLayout>
-
-        
-        <PassageAndQuestionLayout :left-ratio="3" :right-ratio="2" >
-            
         </PassageAndQuestionLayout>
     </div>
 </template>
 <script setup>
 import PassageAndQuestionLayout from './PassageAndQuestionLayout.vue';
+import PassageSummary from './PassageSummaryLayout.vue';
 </script>
 <style scoped>
     
---
+++ src/views/temp/PassageSummaryLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex flex-col items-start p-0 w-full lg:max-w-[35%]">
+    <div class="flex flex-col items-start p-0 gap-4 w-full">
+      <p
+        class="font-pretendard font-bold text-2xl md:text-xl leading-[150%] tracking-[-0.02em] text-[#303030]"
+      >
+        지문 분석
+      </p>
+      <div
+        class="box-border flex flex-col items-start px-6 md:px-8 lg:px-10 py-4 gap-4 w-full bg-white border border-[#E5E7EB] rounded-xl shadow-sm"
+      >
+        <div class="flex flex-row items-center gap-4 w-full">
+          <p
+            class="font-pretendard font-bold text-sm md:text-base leading-[150%] tracking-[-0.02em] text-[#303030] min-w-[80px]"
+          >
+            지문 분야
+          </p>
+          <p
+            class="font-pretendard font-normal text-sm md:text-base leading-[150%] tracking-[-0.02em] text-[#303030] break-words flex-1"
+          >
+            
+            <slot name="subject" />
+          </p>
+        </div>
+        <div class="flex flex-row items-start gap-4 w-full">
+          <p
+            class="font-pretendard font-bold text-sm md:text-base leading-[150%] tracking-[-0.02em] text-[#303030] min-w-[80px] shrink-0"
+          >
+            지문 제재
+          </p>
+          <div class="flex-1 min-w-0">
+            <p
+              class="font-pretendard font-normal text-sm md:text-base leading-[150%] tracking-[-0.02em] text-[#303030] break-words whitespace-pre-wrap overflow-hidden"
+            >
+              
+                <slot name="keyword" />
+            </p>
+          </div>
+        </div>
+        
+        <div class="flex flex-col items-start gap-4 w-full flex-1">
+        <p
+            class="font-pretendard font-bold text-sm md:text-base leading-[150%] tracking-[-0.02em] text-[#303030] min-w-[80px] shrink-0"
+        >
+            핵심 논점
+        </p>
+            <div
+            class="w-full font-pretendard font-normal text-sm md:text-base leading-[200%] tracking-[-0.02em] text-[#303030]"
+            >
+                <slot name="gistText" />
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
