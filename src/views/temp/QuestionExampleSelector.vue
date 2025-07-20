d4e01ab2708403a80d6df15af2d02fdbda6775d0
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-07-20 16:28:20 +0900

    chore: question_example.json의 구조에 맞게 처리하여 필터링한 부분 주석 설명 추가

--- src/views/temp/QuestionExampleSelector.vue
+++ src/views/temp/QuestionExampleSelector.vue
@@ -139,7 +139,7 @@ const questionTypes = ref([
 
 // 1. 기본 questions를 구조별로 필터링
 const questions = computed(() => {
-    // structure 타입 결정
+    // structure 타입 결정, props.generateType에서 공백 제거, question_example.json의 구조에 맞게 처리하여 필터링
     let structureType = props.generateType.replace(/\s/g, '');
     
     // question_example.json에서 구조별 필터링
