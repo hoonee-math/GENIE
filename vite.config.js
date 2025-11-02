61f697daebe9bf2ba2ab856ff705e7763b9dae67
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-11-02 16:04:07 +0900

    dev: 프로젝트 실행용 설정 파일 example 파일 추가

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        port: 80,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/fastapi": {
+                target: "http://localhost:8501",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/fastapi/, ""),
+                secure: false,
+            },
+        },
+    },
+});
